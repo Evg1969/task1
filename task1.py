5dcfc0e5464475dd4233ee9d83dce931ce42454d
Evg1969 <Evg1969@icloud.com>
2024-10-28 14:55:14 +0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+print( 9 ** 0.5 * 5 ) # "1st program"
+print(( 9.99 > 9.98 ) and ( 1000 <= 1000.1 )) # "2st program"
+print( 2 * 2 + 2 ) == ( 2 * (2 + 2)) # "3st program"
+print ( 2 * (2 + 2))
+print((2 * 2 + 2) == 2 * (2 + 2))
+print (int(float ('123.465') * 10 ) % 10) # "4st program"
