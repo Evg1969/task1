a7d0835b9b50b4e736a6f81ac8441a3c2767ffbf
Evg1969 <Evg1969@icloud.com>
2024-11-14 15:59:31 +0800

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,8 @@
+my_string = input("Добрый день")
+print(my_string)
+
+print('Добрый день'.upper())
+print('Добрый день'.lower())
+print('Добрый день'.replace(" ","" ))
+print('Добрый день'[:1])
+print('Добрый день'[10:])
